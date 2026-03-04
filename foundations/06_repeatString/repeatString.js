c71e7a92cf437c532c9a0fe8eaceb77c6e118634
Brian <104478722+9brian@users.noreply.github.com>
2026-03-04 13:00:46 -0800

    Add solutions for problem 6 & test

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,18 @@
-const repeatString = function() {
+const repeatString = function(word, frequency) {
+    
+    let finalString = ""
 
+    if (frequency < 0) {
+        return "ERROR";
+    } else if (frequency == 0) {
+        return finalString;
+    } else {
+        for(let i = 0; i < frequency; i++){
+            finalString += word
+        }
+    
+        return finalString;
+    }
 };
 
 // Do not edit below this line
