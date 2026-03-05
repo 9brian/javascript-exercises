1c1aebc913a700e3a6fb7b8562368db6bdfb581f
Brian <104478722+9brian@users.noreply.github.com>
2026-03-04 22:43:46 -0800

    Include solutions for problem 8

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,20 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+// const removeFromArray = function(list, number) {
+
+    // const newList = []
+
+    // for(let i = 0; i < list.length; i++){
+    //     if (list[i] != number){
+    //         newList.push(list[i]);
+    //     }
+    // }
+
+    // console.log(newList);
+    // return newList;
+
+    
+  return array.filter(val => !args.includes(val))
+
 };
 
 // Do not edit below this line
