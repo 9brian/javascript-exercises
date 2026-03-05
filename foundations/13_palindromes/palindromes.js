ae484f12903b46e2637c6651a750cda106c62664
Brian <104478722+9brian@users.noreply.github.com>
2026-03-05 00:04:22 -0800

    solve problem 13

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,4 +1,42 @@
-const palindromes = function () {
+const palindromes = function (string) {
+
+    let reversedString = ""
+    let cleanedString = ""
+
+    for(let i = 0; i < string.length; i++){
+        if (
+            !(
+            string[i] == "!" || 
+            string[i] == "?" ||
+            string[i] == "." ||
+            string[i] == "," ||
+            string[i] == " "
+        )
+        ){
+            cleanedString += string[i].toLowerCase();
+        }
+        
+    }
+
+    for(let i = string.length - 1; i >= 0; i--){
+        if (
+            !(
+            string[i] == "!" || 
+            string[i] == "?" ||
+            string[i] == "." ||
+            string[i] == "," ||
+            string[i] == " "
+        )
+        ){
+            reversedString += string[i].toLowerCase();
+        }
+        
+    }
+
+    if (reversedString === cleanedString){
+        return true;
+    } return false;
+    // return reversedString;
 
 };
 
