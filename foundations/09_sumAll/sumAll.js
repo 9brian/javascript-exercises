e5edd6fb1359883c0de838729de73b8e55e1cadf
Brian <104478722+9brian@users.noreply.github.com>
2026-03-04 23:00:48 -0800

    Add solutions for problem 9

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,4 +1,35 @@
-const sumAll = function() {
+const sumAll = function(firstNum, secondNum) {
+
+    if (
+        firstNum <= 0 || secondNum <= 0
+        || 
+        Number.isInteger(firstNum) == false
+        ||
+        Number.isInteger(secondNum) == false
+    
+    ){
+        return "ERROR";
+    } 
+
+    
+
+    let finalSum = 0;
+
+    if (firstNum > secondNum){ 
+
+        for(let i = secondNum; i <= firstNum; i++){
+            finalSum += i;
+        }
+
+    } else { // first is less than second
+
+        for(let i = firstNum; i <= secondNum; i++){
+            finalSum += i;
+        }
+    }
+
+
+    return finalSum
 
 };
 
